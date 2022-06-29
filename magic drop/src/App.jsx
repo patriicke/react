78bb128f50d327f48243348a00c6140e0e971e6c
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-29 17:34:02 +0200

    This is a transtion game

--- magic drop/src/App.jsx
+++ magic drop/src/App.jsx
@@ -1,8 +1,23 @@
-import { useEffect } from "react";
+import { useState } from "react";
 
-console.log(1);
-function App() {
-  return <div className="text-red-300">Hello World</div>;
-}
-
+const App = () => {
+  return (
+    <>
+      <div className="flex items-center h-[100vh] justify-center  flex-col gap-2">
+        <div className="group w-[40rem] bg-slate-400 h-[30em] flex flex-col p-2 space-y-8">
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-100 duration-700">1</div>
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-300 duration-700">2</div>
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-500 duration-700">3</div>
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-1000 duration-700">4</div>
+        </div>
+        <div className="group w-[40rem] bg-slate-400 h-[30em] flex flex-col p-2 space-y-8">
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-100 duration-700">4</div>
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-300 duration-700">6</div>
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-500 duration-700">7</div>
+          <div className="w-[10em] h-[20em] bg-white group-hover:translate-x-[29em] transition ease-in-out delay-1000 duration-700">4</div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default App;
